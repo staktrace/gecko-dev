4e2621878e94c6fbbec88d422b34ac4ac6d03d9e
Kartikaya Gupta <kgupta@mozilla.com>
2020-08-26 10:56:17 -0400

    Bug 1662013 - Replace ScrollUpdateInfo with ScrollPositionUpdate. r?botond
    The existing ScrollUpdateInfo that is used to store a "paint-skipped scroll" for
    empty transactions is very similar to the new ScrollPositionUpdate class, so
    we can delete the former and use the latter instead.
    
    The important part of this change is that when applying the pending info to
    a FrameMetrics, we now also append the ScrollUpdateInfo to the mScrollUpdates
    list on the ScrollMetadata. This aligns the code with the previous few patches,
    where we duplicate the scroll information in both the regular FrameMetrics
    fields and the ScrollMetadata::mScrollUpdates array.
    
    Note that the existing code may have a defect when multiple paint-skip scrolls
    occur in a single transaction; only the newest one is kept. In the case where
    the last paint-skip is an absolute scroll, this is fine, but a relative scroll
    may end up clobbering a previous absolute scroll. This patch explicitly detects
    the scenario with multiple paint-skip scrolls and bails out to a full paint
    transaction. As a followup it should be possible to relax this restriction by
    storing an array of pending ScrollPositionUpdate instances.
    
    Differential Revision: https://phabricator.services.mozilla.com/D88743

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -3119,10 +3119,7 @@ void ScrollFrameHelper::ScrollToImpl(nsPoint aPt, const nsRect& aRange,
             // might still get squashed into a full transaction if something
             // happens to trigger one.
             success = manager->SetPendingScrollUpdateForNextTransaction(
-                id,
-                {mScrollGeneration, CSSPoint::FromAppUnits(GetScrollPosition()),
-                 CSSPoint::FromAppUnits(GetApzScrollPosition()),
-                 mLastScrollOrigin == ScrollOrigin::Relative});
+                id, mScrollUpdates.LastElement());
             if (success) {
               schedulePaint = false;
               mOuter->SchedulePaint(nsIFrame::PAINT_COMPOSITE_ONLY);
